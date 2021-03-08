35ff54e5a03d529a026eebe1a418f1a76ca831fa
Kevin Languasco <3322228+kevinalh@users.noreply.github.com>
2021-03-07 20:55:13 -0500

    feat(ch2): make the game use a try loop until guessed

--- 02-guessing-game/guessing_game/src/main.rs
+++ 02-guessing-game/guessing_game/src/main.rs
@@ -1,14 +1,32 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
-    // We use mut to make the variable mutable (since they're immutable by default)
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess) // References are also immutable by default!
-        .expect("Failed to read line");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The secret number is: {}", secret_number);
 
-    println!("You guessed: {}", guess);
+    loop {
+        println!("Please input your guess.");
+        // We use mut to make the variable mutable (since they're immutable by default)
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess) // References are also immutable by default!
+            .expect("Failed to read line");
+
+        // Here we do variable shadowing, usually seen when converting types
+        let guess = guess.trim().parse::<u32>().expect("Please type a number!");
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
