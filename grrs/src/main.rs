e063d3be1c08dd86dfb40d1ef2ea2c64e4d36b5e
Kevin Languasco <kevin+github@languas.co>
2022-02-19 14:32:13 -0500

    refactor(cli): migrate from structopt to clap library

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,23 +1,23 @@
-use structopt::StructOpt;
+use clap::Parser;
 
 // Search for a pattern in a file and display the lines that contain it.
-#[derive(StructOpt)]
+#[derive(Parser)]
 struct Cli {
     // The pattern to look for
     pattern: String,
     // The path to the file to read
-    #[structopt(parse(from_os_str))]
+    #[clap(parse(from_os_str))]
     path: std::path::PathBuf,
 }
 
-fn main() {
-    let args: Cli = Cli::from_args();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Cli = Cli::parse();
     println!("Pattern: {}", args.pattern);
-    println!("Path: {}", args.path.to_str().expect("Could not read file"));
-    let content = std::fs::read_to_string(&args.path).expect("Could not read file");
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
+    Ok(())
 }
